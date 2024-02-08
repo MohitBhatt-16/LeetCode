5b2d5f969bc67f2b0bc3100facb51b4e558afa74
MohitBhatt-16 <mohitbhatt2555@gmail.com>
2024-02-09 00:37:24 +0530

    Percentage of Users Attended a Contest

---
+++ SQL 50/1633 Percentage of Users Attended a Contest.sql
@@ -0,0 +1,4 @@
+Select contest_id, round(count(distinct(user_id))*100/(Select count(user_id) from Users),2) as percentage 
+from Register 
+group by contest_id 
+order by percentage desc, contest_id asc
